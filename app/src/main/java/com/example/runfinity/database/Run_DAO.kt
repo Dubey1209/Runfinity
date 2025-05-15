664aacd0e7ae68d4e5b51b91258f839ef0837d73
dubey <dubeydeepika1209@gmail.com>
2025-05-15 11:53:29 +0530

    Created DAO interface

---
+++ app/src/main/java/com/example/runfinity/database/Run_DAO.kt
@@ -0,0 +1,15 @@
+package com.example.runfinity.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+@Dao
+interface Run_DAO {
+
+
+}
