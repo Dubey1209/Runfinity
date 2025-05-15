c0b6c3d518bb44bb2d0c9d258c23d8a95313a5ed
dubey <dubeydeepika1209@gmail.com>
2025-05-15 11:58:39 +0530

    Updated DAO interface with queries for inserting, deleting, sorting, and aggregating run data

--- app/src/main/java/com/example/runfinity/database/Run_DAO.kt
+++ app/src/main/java/com/example/runfinity/database/Run_DAO.kt
@@ -11,5 +11,36 @@ import androidx.room.Query
 @Dao
 interface Run_DAO {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
 
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query(value = "SELECT* FROM running_table ORDER BY timeStamp")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query(value = "SELECT* FROM running_table ORDER BY timeMillis")
+    fun getAllRunsSortedByTimeMillis(): LiveData<List<Run>>
+
+    @Query(value = "SELECT* FROM running_table ORDER BY burntCalories")
+    fun getAllRunsSortedByBurntCalories(): LiveData<List<Run>>
+
+    @Query(value = "SELECT* FROM running_table ORDER BY distanceMeters")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    @Query(value = "SELECT* FROM running_table ORDER BY averageSpeedKMH ")
+    fun getAllRunsSortedByAverageSpeedKMH(): LiveData<List<Run>>
+
+    @Query(value = "SELECT SUM(timeMillis) FROM running_table")
+    fun getTotalTimeMillis(): LiveData<Long>
+
+    @Query(value = "SELECT SUM(burntCalories) FROM running_table")
+    fun getTotalBurntCalories(): LiveData<Int>
+
+    @Query(value = "SELECT SUM(distanceMeters) FROM running_table")
+    fun getTotalDistanceMeters(): LiveData<Int>
+
+    @Query(value = "SELECT AVERAGE(averageSpeedKMH) FROM running_table")
+    fun getTotalAverageSpeedKMH(): LiveData<Float>
 }
