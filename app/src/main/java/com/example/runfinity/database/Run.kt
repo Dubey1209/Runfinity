9f177b2d17d667246b783a951b70673afe5f75a2
dubey <dubeydeepika1209@gmail.com>
2025-05-16 16:45:45 +0530

    Made necessary kotlin classes Converts and running Database

--- app/src/main/java/com/example/runfinity/database/Run.kt
+++ app/src/main/java/com/example/runfinity/database/Run.kt
@@ -3,22 +3,17 @@ package com.example.runfinity.database
 import android.graphics.Bitmap
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
 
 @Entity(tableName = "running_table")
-data class Run
-    (
-
+data class Run(
+    @TypeConverters(Converters::class)
     var image: Bitmap? = null,
     var timeStamp: Long = 0L,
     var averageSpeedKMH: Float = 0f,
     var distanceMeters: Int = 0,
     var timeMillis: Long = 0L,
-    var burntCalories: Int = 0
-
-)
-
-{
-
+    var burntCalories: Int = 0,
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null
-}
+)
---
+++ app/src/main/java/com/example/runfinity/database/RunningDatabase.kt
@@ -0,0 +1,15 @@
+package com.example.runfinity.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [Run::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
+abstract class RunningDatabase : RoomDatabase() {
+    abstract fun runDao(): Run_DAO
+}
