c588ee018966413c12c5ddc0935a442e46616100
dubey <dubeydeepika1209@gmail.com>
2025-05-18 14:05:26 +0530

    Updated MainActivity.kt file

--- app/src/main/java/com/example/runfinity/ui/MainActivity.kt
+++ app/src/main/java/com/example/runfinity/ui/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.runfinity.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.runfinity.R
 import com.example.runfinity.database.Run_DAO
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,16 +10,11 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-   @Inject
-   lateinit var runDao: Run_DAO
+    @Inject
+    lateinit var runDao: Run_DAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
