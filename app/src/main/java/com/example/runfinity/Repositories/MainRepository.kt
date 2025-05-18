b5511cd2f305e233c280113e3f801bde1bb851b7
dubey <dubeydeepika1209@gmail.com>
2025-05-18 12:22:31 +0530

    Collected all data from data sources (RoomDatabase) in MainRepository.kt

--- app/src/main/java/com/example/runfinity/Repositories/MainRepository.kt
+++ app/src/main/java/com/example/runfinity/Repositories/MainRepository.kt
@@ -1,11 +1,28 @@
 package com.example.runfinity.Repositories
 
+import com.example.runfinity.database.Run
+import com.example.runfinity.database.Run_DAO
+import javax.inject.Inject
 
 
-class MainRepository(
+class MainRepository @Inject constructor(
+     val runDao: Run_DAO
    ) {
 
+    suspend fun insertRun(run: Run) = runDao.insertRun(run)
+    suspend fun deleteRun(run: Run) = runDao.deleteRun(run)
+
+    fun getAllRunSortedByDate() = runDao.getAllRunsSortedByDate()
+    fun getAllRunSortedByDistance() = runDao.getAllRunsSortedByDistance()
+    fun getAllRunSortedByTimeMillis() = runDao.getAllRunsSortedByTimeMillis()
+    fun getAllRunSortedByAvgSpeed() = runDao.getAllRunsSortedByAverageSpeedKMH()
+    fun getAllRunSortedByBurntCalories() = runDao.getAllRunsSortedByBurntCalories()
 
 
 
+    fun getTotalAvgSpeed() = runDao.getTotalAverageSpeedKMH()
+    fun getTotalDistance() = runDao.getTotalDistanceMeters()
+    fun getTotalBurntCalories() = runDao.getTotalBurntCalories()
+    fun getTotalTimeMillis() = runDao.getTotalTimeMillis()
 }
+
